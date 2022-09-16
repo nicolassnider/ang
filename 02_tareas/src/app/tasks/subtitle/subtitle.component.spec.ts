394a7ad800dd637d64ccafdf35c9ed06c3ee7bf1
Nicolás Snider <nsnider@soulitsolutions.com>
2022-09-15 23:12:48 -0300

    termina proyecto 2

--- 02_tareas/src/app/tasks/subtitle/subtitle.component.spec.ts
+++ 02_tareas/src/app/tasks/subtitle/subtitle.component.spec.ts
@@ -4,6 +4,7 @@ import { TaskService } from '../services/tasks.service';
 import { SubtitleComponent } from './subtitle.component';
 
 describe('SubtitleComponent', () => {
+	let service: TaskService;
 	let component: SubtitleComponent;
 	let fixture: ComponentFixture<SubtitleComponent>;
 
@@ -13,6 +14,7 @@ describe('SubtitleComponent', () => {
 			providers:[TaskService],
 		}).compileComponents();
 
+		service = TestBed.inject(TaskService);
 		fixture = TestBed.createComponent(SubtitleComponent);
 		component = fixture.componentInstance;
 		fixture.detectChanges();
@@ -21,5 +23,35 @@ describe('SubtitleComponent', () => {
 	it('should create', () => {
 		expect(component).toBeTruthy();
 	});
+
+	it('should render 3 elements on start',()=>{
+		const render: HTMLElement = fixture.nativeElement;
+		const tasks = render.querySelector('#tasksSubtitle')?.textContent?.trim();
+		expect(tasks).toEqual('Tasks: 3');
+	})
+	it('should decrease by 1 when deleteTask is called',()=>{
+		service.deleteTask('Run');
+		fixture.detectChanges();
+
+		const render: HTMLElement = fixture.nativeElement;
+		const tasks = render.querySelector('#tasksSubtitle')?.textContent?.trim();
+
+		console.log(tasks);
+		expect(tasks).toEqual('Tasks: 2');
+	})
+	it('it should render No available tasks',()=>{
+		service.deleteTask('Run');
+		service.deleteTask('Work');
+		service.deleteTask('Study');
+		console.log(service.tasks)
+		fixture.detectChanges();
+		console.log(service.tasks)
+
+		const render: HTMLElement = fixture.nativeElement;
+		const tasks = render.querySelector('#nonetasks')!.textContent!.trim();
+
+		console.log(tasks);
+		expect(tasks).toEqual('No available tasks');
+	})
 });
 
