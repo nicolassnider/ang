3a98719d12d47dacb876cd96cb7f52201100b1d2
Nicolás Snider <nsnider@soulitsolutions.com>
2022-09-26 17:28:24 -0300

    login and test token

--- 09_rest_mean/controllers/authController.js
+++ 09_rest_mean/controllers/authController.js
@@ -1,23 +1,75 @@
+const bcryptjs = require('bcryptjs');
 const userModel = require('../models/user');
+const jwt = require('jsonwebtoken');
 
-const loginUser = (req, res) => {
-	res.send('auth controller');
+const loginUser = async (req, res) => {
+	const { eMail, password } = req.body;
+	try {
+		const user = await userModel.findOne({ eMail });
+		if (!user) {
+			return res.status(401).json({
+				msg: 'invalid login',
+				ok: false,
+			});
+		}
+		const validPassword = bcryptjs.compareSync(password, user.password);
+		if (!validPassword) {
+			return res.status(401).json({
+				msg: 'invalid login',
+				ok: false,
+			});
+		}
+		const payload = { id: user.id };
+		jwt.sign(
+			payload,
+			process.env.JWT_SECRET,
+			{ expiresIn: 3600 },
+			(error, token) => {
+				res.status(201).json({
+					msg: 'login ok',
+					ok: true,
+					userName: user.userName,
+					id: user.id,
+					token,
+				});
+			}
+		);
+	} catch (error) {
+		return res.status(501).json({ msg: 'internal error', ok: false });
+	}
 };
 const registerUser = async (req, res) => {
 	const { eMail, password, userName } = req.body;
 	try {
 		let user = await userModel.findOne({ eMail });
-		if(user){
-			return res.status(501).json({msg:'email already used',ok:false})
+		if (user) {
+			return res
+				.status(501)
+				.json({ msg: 'email already used', ok: false });
 		}
 		const newUser = new userModel({ eMail, password, userName });
+		const salt = bcryptjs.genSaltSync(10); /* php std is 12 */
+		newUser.password = bcryptjs.hashSync(password, salt);
 		await newUser.save();
-		res.status(201).json({msg:'user created',ok:true,eMail,userName})
+
+		const payload = { id: newUser.id };
+		jwt.sign(
+			payload,
+			process.env.JWT_SECRET,
+			{ expiresIn: 3600 },
+			(error, token) => {
+				res.status(201).json({
+					msg: 'user created',
+					ok: true,
+					userName,
+					id: newUser.id,
+					token,
+				});
+			}
+		);
 	} catch (error) {
-		return res.status(501).json({msg:'internal error',ok:false})
+		return res.status(501).json({ msg: 'internal error', ok: false });
 	}
-	
-	
 };
 
 module.exports = { loginUser, registerUser };
--- 09_rest_mean/routes/auth.js
+++ 09_rest_mean/routes/auth.js
@@ -2,7 +2,7 @@ const {Router} = require('express');
 const { loginUser, registerUser } = require('../controllers/authController');
 const authRouter=Router();
 
-authRouter.get('/login',loginUser);
+authRouter.post('/login',loginUser);
 authRouter.post('/register',registerUser);
 
 module.exports=authRouter;
