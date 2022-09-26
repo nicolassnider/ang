15351d9b2e8fcf4973da47c708c42edcc5010bc4
Nicolás Snider <nsnider@soulitsolutions.com>
2022-09-26 12:46:14 -0300

    instala anfular universal

--- 08_portfolio/src/app/app.module.ts
+++ 08_portfolio/src/app/app.module.ts
@@ -11,7 +11,7 @@ import { SharedModuleModule } from './shared/components/shared.module';
 
   ],
   imports: [
-    BrowserModule,
+    BrowserModule.withServerTransition({ appId: 'serverApp' }),
     AppRoutingModule,SharedModuleModule
   ],
   providers: [],
