91752b87cd8bf69aeb641f07218fe35b8a18c45c
Nicolas Snider <nicolas.snider@outlook.com>
2022-10-07 10:59:07 -0300

    open api doc

--- 09_rest_mean/routes/auth.js
+++ 09_rest_mean/routes/auth.js
@@ -6,29 +6,65 @@ const authRouter = Router();
 /**
  * @swagger
  * components:
- * 	schemas:
- * 		User:
- * 			type: object
- * 			properties:
- * 				userName:
- * 					type: string
- * 					description: The user's name
- * 				eMail:
- * 					type: string
- * 					description: The user's email
- * 				password:
- * 					type: string
- * 					description: The user's password
- * 			required:
- * 				- userName
- * 				- eMail
- * 				- password
- * 			example:
- * 				userName: John Doe
- * 				eMail: example@mail.com
- * 				password: 123456789
+ *      schemas:
+ *          User:
+ *              type: object
+ *              required:
+ *                  - userName
+ *                  - eMail
+ *                  - password
+ *              properties:
+ *                  userName:
+ *                      type: string
+ *                      description: The user's name
+ *                  eMail:
+ *                      type: string
+ *                      description: The user's email
+ *                  password:
+ *                      type: string
+ *                      description: The user's password
+ *              example:
+ *                  userName: johndoe
+ *                  eMail: jdoe@mail.com
+ *                  password: a123456789
+ *          UserLogin:
+ *              type: object
+ *              required:
+ *                  - eMail
+ *                  - password
+ *              properties:
+ *                  eMail:
+ *                      type: string
+ *                      description: The user's email
+ *                  password:
+ *                      type: string
+ *                      description: The user's password
+ *              example:
+ *                  email: jdoe@mail.com
+ *                  password: 123456789
  */
+
+
 authRouter.post(
+ /**
+ * @swagger
+ * /auth/login:
+ *  post:
+ *      summary: Login user
+ *      tags: [User]
+ *      requestBody:
+ *          required: true
+ *          content:
+ *              application/json:
+ *                  schema:
+ *                      type: object
+ *                      $ref: '#/components/schemas/UserLogin'
+ *      responses:
+ *          501:
+ *              description: Server error
+ *          201:
+ *              description: User created
+ */
 	'/login',
 	[
 		check('eMail', 'invalid email').isEmail(),
@@ -37,6 +73,25 @@ authRouter.post(
 	],
 	loginUser
 );
+/**
+ * @swagger
+ * /auth/register:
+ *  post:
+ *      summary: Register new user
+ *      tags: [User]
+ *      requestBody:
+ *          required: true
+ *          content:
+ *              application/json:
+ *                  schema:
+ *                      type: object
+ *                      $ref: '#/components/schemas/User'
+ *      responses:
+ *          501:
+ *              description: Server error
+ *          200:
+ *              description: User created
+ */
 authRouter.post(
 	'/register',
 	[
--- 09_rest_mean/routes/tasks.js
+++ 09_rest_mean/routes/tasks.js
@@ -11,6 +11,55 @@ const validationErrors = require('../middlewares/validationErrors');
 const verifyToken = require('../middlewares/verifyToken');
 const taskRouter = Router();
 
+/**
+ * @swagger
+ * components:
+ *      schemas:
+ *          Task:
+ *              type: object
+ *              required:
+ *                  - name
+ *                  - creator
+ *                  - description
+ *              properties:
+ *                  name:
+ *                      type: string
+ *                      description: The task's name
+ *                  creator:
+ *                      type: string
+ *                      description: The user's id
+ *                  createdAt:
+ *                      type: date
+ *                      description: Time created
+ *                  description:
+ *                      type: string
+ *                      description: Task´s description
+ *              example:
+ *                  name: task365
+ *                  creator: 633258f2e05be2e3d98d68de
+ *                  createdAt: 2022-09-27T23:28:00.601Z
+ *                  desctription: new taks
+ */
+
+ /**
+ * @swagger
+ * /task:
+ *  post:
+ *      summary: Create task
+ *      tags: [Task]
+ *      requestBody:
+ *          required: true
+ *          content:
+ *              application/json:
+ *                  schema:
+ *                      type: object
+ *                      $ref: '#/components/schemas/Task'
+ *      responses:
+ *          501:
+ *              description: Server error
+ *          201:
+ *              description: User created
+ */
 taskRouter.post(
 	'/',
 	[
@@ -20,8 +69,107 @@ taskRouter.post(
 	],
 	createTask
 );
+/**
+ * @swagger
+ * /task:
+ *  get:
+ *      summary: Get tasks list
+ *      tags: [Task]
+ *      parameters:
+ *          - name: x-auth-token
+ *            in: header
+ *            description: JWT Token validation
+ *            required: true
+ *            type: string
+ *      requestBody:
+ *          required: true
+ *          content:
+ *              application/json:
+ *                  schema:
+ *                      type: object
+ *                      properties:
+ *                          uid:
+ *                              type: User
+ *                              description: User id
+ *      responses:
+ *          501:
+ *              description: Server error
+ *          201:
+ *              description: User created
+ */
 taskRouter.get('/', [verifyToken], getTasks);
+
+/**
+ * @swagger
+ * /task/{id}:
+ *  get:
+ *      summary: Get task by id
+ *      tags: [Task]
+ *      parameters:
+ *          - name: x-auth-token
+ *            in: header
+ *            description: JWT Token validation
+ *            required: true
+ *            type: string
+ *          - name: id
+ *            in: path
+ *            description: task id
+ *            required: true
+ *            type: string
+ *      requestBody:
+ *          required: true
+ *          content:
+ *              application/json:
+ *                  schema:
+ *                      type: object
+ *                      properties:
+ *                          uid:
+ *                              type: User
+ *                              description: User id
+ *      responses:
+ *          501:
+ *              description: Server error
+ *          201:
+ *              description: User created
+ */
 taskRouter.get('/:id', [verifyToken], getTask);
+
+/**
+ * @swagger
+ * /task/{id}:
+ *  put:
+ *      summary: modify task
+ *      tags: [Task]
+ *      parameters:
+ *          - name: x-auth-token
+ *            in: header
+ *            description: JWT Token validation
+ *            required: true
+ *            type: string
+ *          - name: id
+ *            in: path
+ *            description: task id
+ *            required: true
+ *            type: string
+ *      requestBody:
+ *          required: true
+ *          content:
+ *              application/json:
+ *                  schema:
+ *                      type: object
+ *                      properties:
+ *                          name:
+ *                              type: string
+ *                              description: task new name
+ *                          description:
+ *                              type: string
+ *                              description: task new description
+ *      responses:
+ *          501:
+ *              description: Server error
+ *          201:
+ *              description: User created
+ */
 taskRouter.put(
 	'/:id',
 	[
@@ -31,6 +179,40 @@ taskRouter.put(
 	],
 	updateTask
 );
+
+/**
+ * @swagger
+ * /task/{id}:
+ *  delete:
+ *      summary: delete task by id
+ *      tags: [Task]
+ *      parameters:
+ *          - name: x-auth-token
+ *            in: header
+ *            description: JWT Token validation
+ *            required: true
+ *            type: string
+ *          - name: id
+ *            in: path
+ *            description: task id
+ *            required: true
+ *            type: string
+ *      requestBody:
+ *          required: true
+ *          content:
+ *              application/json:
+ *                  schema:
+ *                      type: object
+ *                      properties:
+ *                          uid:
+ *                              type: User
+ *                              description: User id
+ *      responses:
+ *          501:
+ *              description: Server error
+ *          201:
+ *              description: User created
+ */
 taskRouter.delete('/:id', [verifyToken], deleteTask);
 
 module.exports = taskRouter;
